b0a04829ee0ac3662456c7ef8d44da918f8b2883
Ton Maxion <ton@maxion.tech>
2023-01-16 17:22:36 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# NFT Sale Contracts
+
+This project is a collection of smart contracts for selling NFTs (ERC1155).
+
+## Setup
+
+1. Install dependencies
+
+```bash
+npm install
+```
+
+2. Compile contracts
+
+```bash
+npx hardhat compile
+```
+
+3. Run tests
+
+```bash
+npx hardhat test
+```
+
+### By Maxion Technologies
---
+++ contracts/NFTSaleContracts.sol
@@ -0,0 +1,374 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.7;
+
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/IERC1155Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+
+/// @custom:security-contact dev@maxion.tech
+contract NFTSaleContracts is
+    Initializable,
+    PausableUpgradeable,
+    AccessControlUpgradeable,
+    ReentrancyGuardUpgradeable
+{
+    using SafeERC20Upgradeable for IERC20Upgradeable;
+
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant PARAMETER_SETTER_ROLE =
+        keccak256("PARAMETER_SETTER_ROLE");
+    bytes32 public constant NFT_SELLER_ROLE = keccak256("NFT_SELLER_ROLE");
+    bytes32 public constant PLATFORM_ROLE = keccak256("PLATFORM_ROLE"); // for withdraw share amount to platform wallet
+    bytes32 public constant PARTNER_ROLE = keccak256("PARTNER_ROLE"); // for withdraw share amount to partner wallet
+
+    uint256 public constant DENOMINATOR = 10**10;
+    uint256 public constant MAX_SHARE_PERCENT = 100 * 10**8;
+
+    uint256 public platformSalesSharePercent;
+    uint256 public partnerSalesSharePercent;
+
+    uint256 public platformSalesShareAmountTotal = 0;
+    uint256 public partnerSalesShareAmountTotal = 0;
+
+    IERC1155Upgradeable public nftContract;
+    IERC20Upgradeable public currencyContract;
+
+    event SetSalesSharePercent(
+        uint256 platformSalesSharePercent,
+        uint256 partnerSalesSharePercent
+    );
+
+    event SetNftToSale(uint256 nftId, uint256 nftQuantity, uint256 nftPrice);
+    event RemoveNftFromSale(uint256 nftId, uint256 nftQuantity);
+    event SetNftPrice(uint256 nftId, uint256 nftPrice);
+    event BuyNft(
+        address buyer,
+        uint256 nftId,
+        uint256 nftQuantity,
+        uint256 nftPrice,
+        uint256 nftTotalPrice,
+        uint256 platformShareAmount,
+        uint256 partnerShareAmount
+    );
+
+    // map is store nft id and nft price
+    mapping(uint256 => uint256) public nftPrice;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(
+        address admin,
+        uint256 initPlatformSalesSharePercent,
+        uint256 initPartnerSalesSharePercent,
+        address nftContractAddress,
+        address currencyContractAddress
+    )
+        external
+        initializer
+        isValidSalesSharePercent(
+            initPlatformSalesSharePercent,
+            initPartnerSalesSharePercent
+        )
+    {
+        // require admin is not zero address
+        require(admin != address(0), "admin is zero address");
+
+        // require admin is not deployer
+        require(admin != msg.sender, "admin is deployer");
+
+        // require nft contract address is not zero address
+        require(
+            nftContractAddress != address(0),
+            "nft contract address is zero address"
+        );
+
+        // require nft contract is valid ERC1155 contract
+        require(
+            IERC1155Upgradeable(nftContractAddress).supportsInterface(
+                type(IERC1155Upgradeable).interfaceId
+            ),
+            "nft contract is not valid ERC1155 contract"
+        );
+
+        // require currency contract address is not zero address
+        require(
+            currencyContractAddress != address(0),
+            "currency contract address is zero address"
+        );
+
+        __Pausable_init();
+        __AccessControl_init();
+        __ReentrancyGuard_init();
+
+        // set admin is DEFAULT_ADMIN_ROLE
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+
+        // set playform and partner share percent
+        platformSalesSharePercent = initPlatformSalesSharePercent;
+        partnerSalesSharePercent = initPartnerSalesSharePercent;
+
+        uint256 platformSalesSharePercentCalculated = (platformSalesSharePercent *
+                100) / DENOMINATOR;
+        uint256 partnerSalesSharePercentCalculated = (partnerSalesSharePercent *
+            100) / DENOMINATOR;
+
+        // emit SetSalesSharePercent event
+        emit SetSalesSharePercent(
+            platformSalesSharePercentCalculated,
+            partnerSalesSharePercentCalculated
+        );
+
+        // set nft contract
+        nftContract = IERC1155Upgradeable(nftContractAddress);
+
+        // set currency contract
+        currencyContract = IERC20Upgradeable(currencyContractAddress);
+    }
+
+    modifier isValidSalesSharePercent(
+        uint256 inputPlatformSalesSharePercent,
+        uint256 inputPartnerSalesSharePercent
+    ) {
+        // require platform and partner share percent is not zero
+        require(inputPlatformSalesSharePercent != 0, "platform share is zero");
+        require(inputPartnerSalesSharePercent != 0, "partner share is zero");
+
+        // require platform and partner share percent is not greater than 100%
+        require(
+            inputPlatformSalesSharePercent <= MAX_SHARE_PERCENT,
+            "platform share is greater than 100%"
+        );
+        require(
+            inputPartnerSalesSharePercent <= MAX_SHARE_PERCENT,
+            "partner share is greater than 100%"
+        );
+
+        // require platform and partner share percent is not greater than 100%
+        require(
+            inputPlatformSalesSharePercent + inputPartnerSalesSharePercent ==
+                MAX_SHARE_PERCENT,
+            "platform and partner share is not equal to 100%"
+        );
+        _;
+    }
+
+    function calculateSalesShareAmount(uint256 amount)
+        public
+        view
+        returns (uint256 platformShareAmount, uint256 partnerShareAmount)
+    {
+        // require amount is not zero
+        require(amount != 0, "amount is zero");
+
+        // calculate platform and partner share amount
+        platformShareAmount =
+            (amount * platformSalesSharePercent) /
+            DENOMINATOR;
+        partnerShareAmount = (amount * partnerSalesSharePercent) / DENOMINATOR;
+    }
+
+    function setSalesShareAmountPercent(
+        uint256 newPlatformSalesSharePercent,
+        uint256 newPartnerSalesSharePercent
+    )
+        external
+        isValidSalesSharePercent(
+            newPlatformSalesSharePercent,
+            newPartnerSalesSharePercent
+        )
+        onlyRole(PARAMETER_SETTER_ROLE)
+    {
+        // set platform and partner share percent
+        platformSalesSharePercent = newPlatformSalesSharePercent;
+        partnerSalesSharePercent = newPartnerSalesSharePercent;
+
+        uint256 platformSalesSharePercentCalculated = (platformSalesSharePercent *
+                100) / DENOMINATOR;
+        uint256 partnerSalesSharePercentCalculated = (partnerSalesSharePercent *
+            100) / DENOMINATOR;
+
+        // emit SetSalesSharePercent event
+        emit SetSalesSharePercent(
+            platformSalesSharePercentCalculated,
+            partnerSalesSharePercentCalculated
+        );
+    }
+
+    // function to transfer nft from seller wallet to this contract and set nft amout to sale and nft price
+    function setNftToSale(
+        uint256 nftId,
+        uint256 nftQuantity,
+        uint256 price
+    ) external onlyRole(NFT_SELLER_ROLE) {
+        // require nft quantity is not zero
+        require(nftQuantity != 0, "nft quantity is zero");
+
+        // require nft price is not zero
+        require(price != 0, "nft price is zero");
+
+        // require nft balance in seller wallet is greater or equal to nft quantity
+        require(
+            nftContract.balanceOf(msg.sender, nftId) >= nftQuantity,
+            "nft balance is less than nft quantity"
+        );
+
+        // require nft id is not zero
+        require(nftId != 0, "nft id is zero");
+
+        // require nft id is not exist
+        require(nftPrice[nftId] == 0, "nft id is exist");
+
+        emit SetNftToSale(nftId, nftQuantity, price);
+
+        // set nft price
+        nftPrice[nftId] = price;
+
+        // transfer nft from seller wallet to this contract
+        nftContract.safeTransferFrom(
+            msg.sender,
+            address(this),
+            nftId,
+            nftQuantity,
+            ""
+        );
+    }
+
+    // function to transfer all nft from this contract back to seller wallet and remove nft price
+    function removeNftFromSale(uint256 nftId)
+        external
+        onlyRole(NFT_SELLER_ROLE)
+    {
+        // require nft id is not zero
+        require(nftId != 0, "nft id is zero");
+
+        // require nft id is exist
+        require(nftPrice[nftId] != 0, "nft id is not exist");
+
+        // get nft amount in this contract
+        uint256 nftAmount = nftContract.balanceOf(address(this), nftId);
+
+        // require nft amount is not zero
+        require(nftAmount != 0, "nft amount is zero");
+
+        emit RemoveNftFromSale(nftId, nftAmount);
+
+        // remove nft price
+        delete nftPrice[nftId];
+
+        // transfer nft from this contract to seller wallet
+        nftContract.safeTransferFrom(
+            address(this),
+            msg.sender,
+            nftId,
+            nftAmount,
+            ""
+        );
+    }
+
+    // function to set nft price
+    function setNftPrice(uint256 nftId, uint256 price)
+        external
+        onlyRole(NFT_SELLER_ROLE)
+    {
+        // require nft id is not zero
+        require(nftId != 0, "nft id is zero");
+
+        // require nft id is exist
+        require(nftPrice[nftId] != 0, "nft id is not exist");
+
+        // require nft price is not zero
+        require(price != 0, "nft price is zero");
+
+        // require nft price is not greater than 1000 ether
+        require(price <= 1000 ether, "nft price is greater than 1000 ether");
+
+        // set nft price
+        nftPrice[nftId] = price;
+
+        emit SetNftPrice(nftId, price);
+    }
+
+    // function to user can buy nft with currency token and transfer nft to user wallet and canculate sales share amount and increase sales share amount of platform and partner to withdraw later
+    function buyNft(uint256 nftId, uint256 nftQuantity)
+        external
+        whenNotPaused
+        nonReentrant
+    {
+        // require nft quantity is not zero
+        require(nftQuantity != 0, "nft quantity is zero");
+
+        // require nft id is not zero
+        require(nftId != 0, "nft id is zero");
+
+        // require nft id is exist
+        require(nftPrice[nftId] != 0, "nft id is not exist");
+
+        // require nft amount in this contract is greater or equal to nft quantity
+        require(
+            nftContract.balanceOf(address(this), nftId) >= nftQuantity,
+            "nft amount is less than nft quantity"
+        );
+
+        // get nft price
+        uint256 nftPriceForSale = nftPrice[nftId];
+        // calculate nft total price
+        uint256 nftTotalPrice = nftPriceForSale * nftQuantity;
+
+        // require user have enough currency token to buy nft
+        require(
+            currencyContract.balanceOf(msg.sender) >= nftTotalPrice,
+            "user have not enough currency token to buy nft"
+        );
+
+        // calculate sales share amount
+        (
+            uint256 platformShareAmount,
+            uint256 partnerShareAmount
+        ) = calculateSalesShareAmount(nftTotalPrice);
+
+        emit BuyNft(
+            msg.sender,
+            nftId,
+            nftQuantity,
+            nftPriceForSale,
+            nftTotalPrice,
+            platformShareAmount,
+            partnerShareAmount
+        );
+
+        // increase sales share amount of platform and partner to withdraw later
+        platformSalesShareAmountTotal += platformShareAmount;
+        partnerSalesShareAmountTotal += partnerShareAmount;
+
+        // transfer nft from this contract to user wallet
+        nftContract.safeTransferFrom(
+            address(this),
+            msg.sender,
+            nftId,
+            nftQuantity,
+            ""
+        );
+
+        // transfer currency token from user wallet to this contract
+        currencyContract.safeTransferFrom(
+            msg.sender,
+            address(this),
+            nftTotalPrice
+        );
+    }
+
+    function pause() external onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() external onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+}
