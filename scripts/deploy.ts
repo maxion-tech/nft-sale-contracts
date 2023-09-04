99a0beee51259adb0a3def4e1a2cfc4cad98088d
Ton Maxion <ton@maxion.tech>
2023-09-04 14:57:35 +0700

    add deploy scripts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,5 +1,4 @@
-import hre, { ethers, upgrades } from "hardhat";
-import { NFTSaleContract } from "../typechain-types";
+import hre, { ethers } from "hardhat";
 
 async function main() {
   const NFTSaleContractFactory = await ethers.getContractFactory("NFTSaleContract");
@@ -11,14 +10,14 @@ async function main() {
   // check if currency contract address is set
   if (!CURRENCY_CONTRACT_ADDRESS) throw new Error("CURRENCY_CONTRACT_ADDRESS is not set");
 
-  const PLATFORM_SALES_SHARE_PERCENT = 40 * 10 ** 8;
-  const PARTNER_SALES_SHARE_PERCENT = 60 * 10 ** 8;
+  const PLATFORM_SALES_SHARE_PERCENT = 55 * 10 ** 8;
+  const PARTNER_SALES_SHARE_PERCENT = 45 * 10 ** 8;
 
-  const nftSaleContract = await upgrades.deployProxy(NFTSaleContractFactory, [ADMIN_ADDRESS, PLATFORM_SALES_SHARE_PERCENT, PARTNER_SALES_SHARE_PERCENT, NFT_ADDRESS, CURRENCY_CONTRACT_ADDRESS]) as NFTSaleContract;
+  const nftSaleContract = await NFTSaleContractFactory.deploy(ADMIN_ADDRESS, PLATFORM_SALES_SHARE_PERCENT, PARTNER_SALES_SHARE_PERCENT, NFT_ADDRESS, CURRENCY_CONTRACT_ADDRESS);
   await nftSaleContract.deployed();
 
   console.log("NFTSaleContract deployed to:", nftSaleContract.address);
-  console.log(`You can verify contract by running: npx hardhat verify --network ${hre.network.name} ${nftSaleContract.address}`);
+  console.log(`You can verify contract by running: npx hardhat verify --network ${hre.network.name} ${nftSaleContract.address} ${ADMIN_ADDRESS} ${PLATFORM_SALES_SHARE_PERCENT} ${PARTNER_SALES_SHARE_PERCENT} ${NFT_ADDRESS} ${CURRENCY_CONTRACT_ADDRESS}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
